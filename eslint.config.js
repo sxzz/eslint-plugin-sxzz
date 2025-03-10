fb594300b4f5be71e4232da39b38832f1eada0b9
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-10 23:21:49 +0800

    chore: upgrade deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import { sxzz } from '@sxzz/eslint-config'
 
-export default sxzz({
+export default sxzz().append({
   rules: {
     'import/no-default-export': 'off',
     'sxzz/require-package-field': 'error',
